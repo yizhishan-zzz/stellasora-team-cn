b127590a397006a857449c97225f5e7b3bc8fb6f
site <site@local>
2026-09-18 19:53:43 +0800

    清理调试脚本

--- tools/_find.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const t = fs.readFileSync('assets/js/main.js', 'utf8');
-const L = t.split(String.fromCharCode(10));
-console.log('=== 含 STYLE_BTN / POWER_BTN 等的行 ===');
-L.forEach((l, i) => {
-  if (/STYLE_BTN|POWER_BTN|STYLE_MAP|POWER_MAP/.test(l)) {
-    console.log('  行' + (i + 1) + ': ' + JSON.stringify(l));
-  }
-});
